7090730f200dd14b2aa4d3eaa77555d0599e69f4
John Nyokabi <jwnyokabi@gmail.com>
2023-07-06 02:24:13 -0700

    ES6 javascript promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignUp() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then(([photoRes, userRes]) => {
-      const { body: photo } = photoRes;
-      const { firstName, lastName } = userRes;
-      console.log(`${photo} ${firstName} ${lastName}`);
-    })
-    .catch(() => {
-      console.log('Signup system offline ');
-    });
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((data) => console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`),
+      () => console.log('Signup system offline'));
 }
