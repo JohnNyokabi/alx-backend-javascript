a073b999f364de07bfd978a3f74e3f31094694d8
John Nyokabi <jwnyokabi@gmail.com>
2023-08-25 07:33:49 +0300

    node basics

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,14 +1,6 @@
 const express = require('express');
+
 const app = express();
-
-const port = 1245;
-
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!\n');
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
+app.get('/', (req, res) => res.send('Hello Holberton School!'));
+app.listen(1245);
 module.exports = app;
--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,28 +1,16 @@
 const express = require('express');
-const { countStudents } = require('./3-read_file_async');
+const countStudents = require('./3-read_file_async');
 
 const app = express();
-const port = 1245;
-const dbPath = process.argv[2];
-
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!\n');
+app.get('/', (req, res) => res.send('Hello Holberton School!'));
+app.get('/students', async (req, res) => {
+  const title = 'This is the list of our students\n';
+  try {
+    const data = await countStudents(process.argv[2]);
+    res.send(`${title}${data.join('\n')}`);
+  } catch (error) {
+    res.send(`${title}${error.message}`);
+  }
 });
-
-app.get('/students', (req, res) => {
-  res.write('This is the list of our students\n');
-
-  countStudents(dbPath)
-    .then(() => {
-      res.end();
-    })
-    .catch((error) => {
-      res.end(error.message);
-    });
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
+app.listen(1245);
 module.exports = app;
